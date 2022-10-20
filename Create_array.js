76e3df981c794004094f2660341a2daf62597975
yashbomble <yashbomble2002@gmail.com>
2022-10-20 17:50:57 +0530

    initial commit

--- Create_array.js
+++ Create_array.js
@@ -1,8 +1,12 @@
-let arr=['a','b','c','d','e'];
 //array is created using let keyword but always use const
 console.log('Array using let :');
+let arr=['a','b','c','d','e'];
 console.log(arr);
+
 console.log();
+
+//Always use const to create array its actual meaning is not applicable on the array
 console.log('Array using const :')
 const array=['A','B','C','D','E'];
+
 console.log(array);
