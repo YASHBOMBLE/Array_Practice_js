46f7811bf8b8fca34239b3bfc6bec00ec58e2664
yashbomble <yashbomble2002@gmail.com>
2022-10-22 19:00:31 +0530

    array_forlooppage

---
+++ array_forloop.js
@@ -0,0 +1,6 @@
+const array=['yash','Aniket','vishal','Avi'];
+for(let i=0;i<array.length;i++)
+{
+    console.log('Message sent',array[i]);
+}
+//to perform some action on perticular array elements use for loop,forEach() map functions
